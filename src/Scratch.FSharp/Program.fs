b2cb05b631089e29e65ea2553c7c1b9a47351789
Stefan Maierhofer <sm@stefanmaierhofer.com>
2020-08-29 08:27:46 +0200

    add Scratch.FSharp.fsproj

---
+++ src/Scratch.FSharp/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+open Aardvark.Base
+open Aardvark.Data
+
+[<EntryPoint>]
+let main argv =
+
+    #if NETCOREAPP3_1
+    printfn "NETCOREAPP3_1"
+    #endif
+
+    #if NET472
+    printfn "NET472"
+    #endif
+
+    let buffer = DurableCodec.Serialize(Durable.Aardvark.Cell2d, Cell2d(1, 2, 3));
+    printfn "buffer.Length = %d" buffer.Length
+    printfn "%s" (String.Join(" ", buffer |> Array.map (fun x -> x.ToString())))
+
+    0
