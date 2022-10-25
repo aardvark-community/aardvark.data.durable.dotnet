dbcba4736e49703dcc24c3abd4edb95340bcdb77
Stefan Maierhofer <sm@stefanmaierhofer.com>
2022-10-25 08:20:26 +0200

    fix pushpackages_nuget.cmd

--- pushpackages_nuget.cmd
+++ pushpackages_nuget.cmd
@@ -12,4 +12,3 @@ dotnet paket pack bin --version %1
 set /p Key=<%HOMEPATH%\.ssh\nuget.key
 dotnet nuget push "bin\Aardvark.Data.Durable.%1.nupkg" --skip-duplicate --source https://www.nuget.org/api/v2/package -k %Key%
 dotnet nuget push "bin\Aardvark.Data.Durable.Codec.%1.nupkg" --skip-duplicate --source https://www.nuget.org/api/v2/package -k %Key%
-dotnet nuget push "bin\Aardworx.Data.DurableCodec2.%1.nupkg" --skip-duplicate --source https://www.nuget.org/api/v2/package -k %Key%
